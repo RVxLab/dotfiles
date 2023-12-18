48486e987bade88499e4a140674de300bd80522a
RVxLab <richard@rvx.works>
2023-12-19 09:06:46 +1000

    Start setting up LSP

--- dotfiles/vim/.config/nvim/lua/packages/lsp.lua
+++ dotfiles/vim/.config/nvim/lua/packages/lsp.lua
@@ -1,2 +1,27 @@
-return {}
+return {
+    { 'williamboman/mason.nvim', },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        dependencies = {
+            'williamboman/mason.nvim',
+        },
+        config = {
+            ensure_installed = {
+                'lua_ls',
+                'html',
+                'tsserver',
+                'intelephense', -- PHP
+                'rust_analyzer',
+                -- 'tailwindcss',
+                -- 'cssls',
+            },
+        },
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason-lspconfig.nvim',
+        },
+    },
+}
 
