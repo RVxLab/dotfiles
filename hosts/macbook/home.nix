fc7bfd34f5e02431b95acfa30db7dc38fceb75e5
RVxLab <code@rvx.works>
2024-04-14 15:21:48 +1000

    Add Bob to PATH

--- hosts/macbook/home.nix
+++ hosts/macbook/home.nix
@@ -4,6 +4,11 @@
     inherit username;
     homeDirectory = "/Users/${username}";
     stateVersion = "23.11";
+
+    # Additional PATHs
+    sessionPath = [
+      "$HOME/.local/share/bob/nvim-bin"
+    ];
   };
 
   # Allow Home Manager to install and manage itself
