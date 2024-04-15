dbe147c060cd3ee12d785daeb9b94d41a1c8c029
RVxLab <code@rvx.works>
2024-04-15 17:59:20 +1000

    Move more programs from Homebrew to Nix

--- Brewfile
+++ Brewfile
@@ -1,31 +1,6 @@
 # Taps
 tap "homebrew/cask"
 tap "homebrew/cask-fonts"
-tap "oven-sh/bun"
-
-# Formulea
-brew "bat"
-brew "bun"
-brew "curl"
-brew "difftastic"
-brew "dust"
-brew "eza"
-brew "fd"
-brew "git-delta"
-brew "gitui"
-brew "httpie"
-brew "jq"
-brew "just"
-brew "lazygit"
-brew "podman"
-brew "podman-compose"
-brew "ripgrep"
-brew "starship"
-brew "stow"
-brew "tre-command"
-brew "wget"
-brew "zellij"
-brew "zoxide"
 
 # Casks
 cask "arc"
--- modules/common/shell.nix
+++ modules/common/shell.nix
@@ -1,143 +1,171 @@
-{ vars, ... }:
+{ vars, pkgs, ... }:
 {
-  home-manager.users.${vars.username}.programs = {
-    direnv = {
-      # Enable Direnv
-      enable = true;
-      nix-direnv.enable = true;
+  home-manager.users.${vars.username} = {
+    home.packages = with pkgs; [
+      # Basics
+      curl
+      wget
 
-      # Integrate with Zsh
-      enableZshIntegration = true;
-    };
+      # General CLI utils
+      bat # Better cat
+      eza # Better ls
+      dust # Better du
+      fd # Better find
+      httpie # User-friendly HTTP tool
+      just # Task runner
+      jq # JSON manipulation
+      ripgrep # Better grep
+      tre-command # Better tree
+      zoxide # Jump to known directories
 
-    # Allow Home Manager to manage zsh
-    zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
+      # Git
+      difftastic # Diff and with syntax highlighting
+      delta # Git pager
+      lazygit # Git CLI tool
 
-      # Set up aliases
-      shellAliases = {
-        # General aliases
-        ls = "eza -lag --header";
-        ll = "eza -lag --header";
-        cat = "bat";
-        fzf = "fzf --preview 'bat {}'";
-        lg = "lazygit";
+      # Shell
+      starship
+    ];
 
-        # PHP
-        a = "php artisan";
+    programs = {
+      direnv = {
+        # Enable Direnv
+        enable = true;
+        nix-direnv.enable = true;
+
+        # Integrate with Zsh
+        enableZshIntegration = true;
       };
-    };
 
-    # Install fzf
-    fzf = {
-      enable = true;
-      enableZshIntegration = true;
-    };
+      # Allow Home Manager to manage zsh
+      zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
 
-    # Install Zoxide
-    zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-    };
+        # Set up aliases
+        shellAliases = {
+          # General aliases
+          ls = "eza -lag --header";
+          ll = "eza -lag --header";
+          cat = "bat";
+          fzf = "fzf --preview 'bat {}'";
+          lg = "lazygit";
 
-    # Install Starship
-    starship = {
-      enable = true;
-      enableZshIntegration = true;
-      settings = {
-        add_newline = false;
-        format = "$username$hostname$directory$git_branch$git_state$git_status$git_metrics$fill\${custom.laravel}$php$rust$nodejs$cmd_duration $jobs $time$line_break$character";
-
-        nodejs = {
-          format = "[$symbol($version )]($style)";
+          # PHP
+          a = "php artisan";
         };
+      };
 
-        php = {
-          format = "[$symbol($version )]($style)";
-        };
+      # Install fzf
+      fzf = {
+        enable = true;
+        enableZshIntegration = true;
+      };
 
-        fill = {
-          symbol = " ";
-        };
+      # Install Zoxide
+      zoxide = {
+        enable = true;
+        enableZshIntegration = true;
+      };
 
-        directory = {
-          style = "blue";
-          read_only = " ";
-          truncation_length = 4;
-          truncate_to_repo = false;
-        };
+      # Install Starship
+      starship = {
+        enable = true;
+        enableZshIntegration = true;
+        settings = {
+          add_newline = false;
+          format = "$username$hostname$directory$git_branch$git_state$git_status$git_metrics$fill\${custom.laravel}$php$rust$nodejs$cmd_duration $jobs $time$line_break$character";
 
-        character = {
-          success_symbol = "[❯](blue)";
-          error_symbol = "[❯](red)";
-          vicmd_symbol = "[❮](green)";
-        };
-
-        git_branch = {
-          symbol = " ";
-          format = "[$symbol$branch]($style) ";
-          style = "bright-black";
-        };
-
-        jobs = {
-          symbol = "";
-          style = "bold red";
-          number_threshold = 1;
-          format = "[$symbol]($style)";
-        };
-
-        git_status = {
-          style = "cyan";
-        };
-
-        git_state = {
-          format = "'\([$state( $progress_current/$progress_total)]($style)\) '";
-          style = "bright-black";
-        };
-
-        git_metrics = {
-          disabled = false;
-        };
-
-        cmd_duration = {
-          format = "[$duration]($style)";
-          style = "yellow";
-        };
-
-        memory_usage = {
-          symbol = " ";
-        };
-
-        rust = {
-          symbol = " ";
-          format = "[$symbol($version )]($style)";
-        };
-
-        time = {
-          disabled = false;
-          style = "bold white";
-          format = "[$time ]($style)";
-        };
-
-        # Custom entries
-        custom = {
-          laravel = {
-            when = "test -d vendor/laravel/framework";
-            description = "Shows the current Laravel version";
-            style = "bold red";
-            symbol = "L";
-            command = "jq -r '.packages[] | select(.name == \"laravel/framework\") | .version' composer.lock";
-            format = "[$symbol $output ]($style)";
+          nodejs = {
+            format = "[$symbol($version )]($style)";
           };
 
-          stunnel = {
-            when = "ps aux | grep stunnel | grep -v grep";
-            command = "ps -o etime= -p $(ps aux | grep stunnel | grep -v grep | awk '{print $2}')";
-            style = "red";
-            format = "[TUNNEL OPEN for $output]($style)";
+          php = {
+            format = "[$symbol($version )]($style)";
+          };
+
+          fill = {
+            symbol = " ";
+          };
+
+          directory = {
+            style = "blue";
+            read_only = " ";
+            truncation_length = 4;
+            truncate_to_repo = false;
+          };
+
+          character = {
+            success_symbol = "[❯](blue)";
+            error_symbol = "[❯](red)";
+            vicmd_symbol = "[❮](green)";
+          };
+
+          git_branch = {
+            symbol = " ";
+            format = "[$symbol$branch]($style) ";
+            style = "bright-black";
+          };
+
+          jobs = {
+            symbol = "";
+            style = "bold red";
+            number_threshold = 1;
+            format = "[$symbol]($style)";
+          };
+
+          git_status = {
+            style = "cyan";
+          };
+
+          git_state = {
+            format = "'\([$state( $progress_current/$progress_total)]($style)\) '";
+            style = "bright-black";
+          };
+
+          git_metrics = {
+            disabled = false;
+          };
+
+          cmd_duration = {
+            format = "[$duration]($style)";
+            style = "yellow";
+          };
+
+          memory_usage = {
+            symbol = " ";
+          };
+
+          rust = {
+            symbol = " ";
+            format = "[$symbol($version )]($style)";
+          };
+
+          time = {
+            disabled = false;
+            style = "bold white";
+            format = "[$time ]($style)";
+          };
+
+          # Custom entries
+          custom = {
+            laravel = {
+              when = "test -d vendor/laravel/framework";
+              description = "Shows the current Laravel version";
+              style = "bold red";
+              symbol = "L";
+              command = "jq -r '.packages[] | select(.name == \"laravel/framework\") | .version' composer.lock";
+              format = "[$symbol $output ]($style)";
+            };
+
+            stunnel = {
+              when = "ps aux | grep stunnel | grep -v grep";
+              command = "ps -o etime= -p $(ps aux | grep stunnel | grep -v grep | awk '{print $2}')";
+              style = "red";
+              format = "[TUNNEL OPEN for $output]($style)";
+            };
           };
         };
       };
