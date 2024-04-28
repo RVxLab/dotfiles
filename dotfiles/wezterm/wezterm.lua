e7755f7a1bad89d29257034c590b9069716a38fe
RVxLab <code@rvx.works>
2024-04-28 21:02:10 +1000

    Wezterm config

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -6,6 +6,7 @@ local config = wezterm.config_builder()
 -------------------------------------------------------------------------------
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 13.0
+config.line_height = 1.3
 
 -- Fix fonts looking really thin
 config.freetype_load_flags = "NO_HINTING"
@@ -14,5 +15,20 @@ config.freetype_load_flags = "NO_HINTING"
 -- Color scheme
 -------------------------------------------------------------------------------
 config.color_scheme = "Catppuccin Frappe"
+
+-------------------------------------------------------------------------------
+-- Keymap
+-------------------------------------------------------------------------------
+config.keys = {}
+
+-------------------------------------------------------------------------------
+-- Window settings
+-------------------------------------------------------------------------------
+config.window_padding = {
+    top = 0,
+    right = 0,
+    bottom = 0,
+    left = 0,
+}
 
 return config
