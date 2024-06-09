ee42bd74d938f495dba17d9993ec1f5dfce48c45
RVxLab <code@rvx.works>
2024-06-09 17:21:57 +1000

    Increase font size to 15pt

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 -- Fonts
 -------------------------------------------------------------------------------
 config.font = wezterm.font("JetbrainsMono Nerd Font")
-config.font_size = 14.0
+config.font_size = 15.0
 config.line_height = 1.4
 
 -------------------------------------------------------------------------------
