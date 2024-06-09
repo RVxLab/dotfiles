6250c3e239c82219d3aba1494408703ae4da85da
RVxLab <code@rvx.works>
2024-06-09 17:23:02 +1000

    Add a shortcut to split a pane based on size
    A simple splitting method to will attempt to split in the "longest"
    direction
    
    If the pane is longer than it is wide, it will try to split downwards.
    Otherwise it will split to the right

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -16,7 +16,40 @@ config.color_scheme = "Catppuccin Macchiato"
 -------------------------------------------------------------------------------
 -- Keymap
 -------------------------------------------------------------------------------
-config.keys = {}
+config.keys = {
+    {
+        key = "Enter",
+        mods = "CMD",
+        ---@diagnostic disable-next-line: unused-local
+        action = wezterm.action_callback(function(_window, pane)
+            local active_pane_info = nil
+
+            for _, pane_with_info in ipairs(pane:tab():panes_with_info()) do
+                if pane_with_info.pane:pane_id() == pane:pane_id() then
+                    active_pane_info = pane_with_info
+                end
+            end
+
+            if active_pane_info == nil then
+                return
+            end
+
+            local width = active_pane_info.pixel_width
+            local height = active_pane_info.pixel_height
+
+            wezterm.log_info({
+                width = width,
+                height = height,
+            })
+
+            local split_direction = height >= width and "Bottom" or "Right"
+
+            pane:split({
+                direction = split_direction,
+            })
+        end),
+    },
+}
 
 -------------------------------------------------------------------------------
 -- Window settings
