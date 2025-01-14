95e73f5a808d981d49a4b582674e2f330cce7be7
RVxLab <code@rvx.works>
2025-01-14 13:09:38 +1000

    Install Fish and Fisher
    Further setup and launch using Kitty

---
+++ dotfiles/fish/.config/fish/config.fish
@@ -0,0 +1,59 @@
+# Check if we're on macOS
+set -l system "$(uname -s)"
+
+if test "$system" = "Darwin"
+    set -f is_darwin 1
+else
+    set -f is_darwin 0
+end
+
+# Set up default editor
+set -gx EDITOR "nvim"
+
+# Abbreviations
+abbr -a a php artisan
+abbr -a lg lazygit
+abbr -a fzf fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'
+abbr -a ll eza -lag --header
+abbr -a s kitten ssh
+
+# Add path
+fish_add_path "$HOME/bin"
+
+# Add paths only on macOS
+if test "$is_darwin" = 1
+    fish_add_path "$HOME/.docker/bin"
+
+    # Add XDG_CONFIG if it's not already set
+    if test -z "$XDG_CONFIG_HOME"
+        set -gx XDG_CONFIG_HOME "$HOME/.config"
+    end
+
+    # Homebrew
+    fish_add_path "/opt/homebrew/bin"
+    set -gx HOMEBREW_BUNDLE_FILE_GLOBAL "$HOME/.dotfiles/Brewfile"
+
+    # Laravel Herd
+    set -gx HERD_PHP_83_INI_SCAN_DIR "$HOME/Library/Application Support/Herd/config/php/83"
+    set -gx HERD_PHP_84_INI_SCAN_DIR "$HOME/Library/Application Support/Herd/config/php/84"
+
+    fish_add_path "$HOME/Library/Application Support/Herd/bin"
+end
+
+# pnpm
+set -gx PNPM_HOME "$HOME/Library/pnpm"
+fish_add_path "$PNPM_HOME"
+# pnpm end
+
+# Interactive stuff
+if status is-interactive
+    # Initialize CLI tooling
+    starship init fish | source
+    fzf --fish | source
+    zoxide init fish | source
+
+    if test "$is_darwin" = 1
+        bob complete fish | source
+        fish_add_path "$HOME/.local/share/bob/nvim-bin"
+    end
+end
